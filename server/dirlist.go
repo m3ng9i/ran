6c8749eb3f3c84d11cb4f4ce96d7843477155f97
Junaid Khalid <junaid.khalid@outlook.com>
2015-10-03 17:44:53 +0500

    added human readable file size in dir listing. changed dependency in readme.md

--- server/dirlist.go
+++ server/dirlist.go
@@ -9,21 +9,19 @@ import "net/url"
 import "html"
 import "path"
 import "path/filepath"
-
+import "github.com/dustin/go-humanize"
 
 type dirListFiles struct {
-    Name        string
-    Url         string
-    Size        int64
-    ModTime     time.Time
+	Name    string
+	Url     string
+	Size    string
+	ModTime time.Time
 }
-
 
 type dirList struct {
-    Title string
-    Files []dirListFiles
+	Title string
+	Files []dirListFiles
 }
-
 
 const dirListTpl = `<!DOCTYPE HTML>
 <html>
@@ -37,7 +35,29 @@ const dirListTpl = `<!DOCTYPE HTML>
 body {
     background-color:white;
     color: #333333;
+    font-family:"Source Sans Pro",sans-serif;
+
 }
+
+a:link {
+    color: #065588;
+}
+
+/* visited link */
+a:visited {
+    color: #2090D8;
+}
+
+/* mouse over link */
+a:hover {
+    color: #0C80CA;
+}
+
+/* selected link */
+a:active {
+    color: #0000FF;
+}
+
 
 table {
     border-collapse: collapse;
@@ -50,7 +70,7 @@ table tr:nth-child(1) {
 table th, table td {
     padding: 8px 10px;
     border:1px #dddddd solid;
-    font-size: 14px;
+    font-size: 15px;
 }
 
 table a {
@@ -62,7 +82,7 @@ table tr:hover {
 }
 
 table tr > td:nth-child(2), table tr > td:nth-child(3) {
-    font-size: 13px;
+    font-size: 14px;
 }
 
 </style>
@@ -86,94 +106,88 @@ table tr > td:nth-child(2), table tr > td:nth-child(3) {
 </body>
 </html>`
 
-
 var tplDirList *template.Template
 
-
 func timeToString(t time.Time, format ...string) string {
-    f := "2006-01-02 15:04:05"
-    if len(format) > 0 && format[0] != "" {
-        f = format[0]
-    }
-    return t.Format(f)
+	f := "2006-01-02 15:04:05"
+	if len(format) > 0 && format[0] != "" {
+		f = format[0]
+	}
+	return t.Format(f)
 }
-
 
 func init() {
-    var err error
-    tplDirList = template.New("dirlist").Funcs(template.FuncMap{"t2s": timeToString})
-    tplDirList, err = tplDirList.Parse(dirListTpl)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Directory list template init error: %s", err.Error())
-        os.Exit(1)
-    }
+	var err error
+	tplDirList = template.New("dirlist").Funcs(template.FuncMap{"t2s": timeToString})
+	tplDirList, err = tplDirList.Parse(dirListTpl)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Directory list template init error: %s", err.Error())
+		os.Exit(1)
+	}
 }
-
 
 // List content of a directory.
 // If error occurs, this function will return an error and won't write anything to ResponseWriter.
 func (this *RanServer) listDir(w http.ResponseWriter, c *context) (size int64, err error) {
 
-    if !c.exist {
-        size = Error(w, 404)
-        return
-    }
+	if !c.exist {
+		size = Error(w, 404)
+		return
+	}
 
-    if !c.isDir {
-        err = fmt.Errorf("Cannot list contents of a non-directory")
-        return
-    }
+	if !c.isDir {
+		err = fmt.Errorf("Cannot list contents of a non-directory")
+		return
+	}
 
-    f, err := os.Open(c.absFilePath)
-    if err != nil {
-        return
-    }
-    defer f.Close()
+	f, err := os.Open(c.absFilePath)
+	if err != nil {
+		return
+	}
+	defer f.Close()
 
-    info, err := f.Readdir(0)
-    if err != nil {
-        return
-    }
+	info, err := f.Readdir(0)
+	if err != nil {
+		return
+	}
 
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-    title := html.EscapeString(path.Base(c.cleanPath))
+	title := html.EscapeString(path.Base(c.cleanPath))
 
-    var files []dirListFiles
+	var files []dirListFiles
 
-    for n, i := range info {
-        // TODO: skipped files according to ignore list in config
-        name := i.Name()
-        if i.IsDir() {
-            name += "/"
-        }
-        name = html.EscapeString(name)
-        url := url.URL{Path: name}
+	for n, i := range info {
+		// TODO: skipped files according to ignore list in config
+		name := i.Name()
+		if i.IsDir() {
+			name += "/"
+		}
+		name = html.EscapeString(name)
+		url := url.URL{Path: name}
 
-        // write parent dir
-        if n == 0 && c.cleanPath != "/" {
-            parent := c.parent()
+		// write parent dir
+		if n == 0 && c.cleanPath != "/" {
+			parent := c.parent()
 
-            var info os.FileInfo
-            info, err = os.Stat(filepath.Join(this.config.Root, parent))
-            if err != nil {
-                return
-            }
+			var info os.FileInfo
+			info, err = os.Stat(filepath.Join(this.config.Root, parent))
+			if err != nil {
+				return
+			}
 
-            files = append(files, dirListFiles{Name:"[..]", Url:parent, ModTime:info.ModTime()})
-        }
+			files = append(files, dirListFiles{Name: "[..]", Url: parent, ModTime: info.ModTime()})
+		}
 
-        files = append(files, dirListFiles{Name:name, Url:url.String(), Size:i.Size(), ModTime:i.ModTime()})
-    }
+		files = append(files, dirListFiles{Name: name, Url: url.String(), Size: humanize.Bytes(uint64(i.Size())), ModTime: i.ModTime()})
+	}
 
-    data := dirList{ Title: title, Files: files}
+	data := dirList{Title: title, Files: files}
 
-    buf := bufferPool.Get()
-    defer bufferPool.Put(buf)
+	buf := bufferPool.Get()
+	defer bufferPool.Put(buf)
 
-    tplDirList.Execute(buf, data)
-    size, _ = buf.WriteTo(w)
-    return
+	tplDirList.Execute(buf, data)
+	size, _ = buf.WriteTo(w)
+	return
 }
-
-
